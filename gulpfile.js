72574a7147fc1e882d090fe4b2cfa9f3fff9769e
keenghost <china-kare@hotmail.com>
2020-10-14 18:43:16 +0800

    ci: update version to v0.1.2

--- gulpfile.js
+++ gulpfile.js
@@ -64,7 +64,7 @@ function makeWinInstaller(options) {
 }
 
 function packWinInstaller() {
-  const version = '0.1.1.0'
+  const version = '0.1.2.0'
 
   return makeWinInstaller({
     name: 'StartGuildwars2',
